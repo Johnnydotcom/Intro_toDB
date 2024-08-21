9d7a84302aa63fa09d0521d8c3f13ee0812f338c
qayiya tshotsho <mr.qtshotsho@gmail.com>
2024-08-21 23:13:52 +0200

    working with databases.

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,25 +1,27 @@
 import mysql.connector
 
 # Replace with your connection details
-mydb = mysql.connector.connect(
+try :
+    mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     password="root",
     database="alx_book_store"
 )
 
-#print(mydb.get_server_info())
+    #print(mydb.get_server_info())
 
-mycursor = mydb.cursor()
+    mycursor = mydb.cursor()
 
-mycursor.execute("""
-CREATE DATABASE IF NOT EXISTS alx_book_store;
-""")
+    mycursor.execute("""
+    CREATE DATABASE IF NOT EXISTS alx_book_store;
+    """)
 
-print("Database 'alx_book_store' created successfully!")
-
-mycursor.close()
-mydb.close()
-
-print("Database connection closed.")
+    print("Database 'alx_book_store' created successfully!")
+except Exception:
+    mysql.connector.Error
+finally :
+    mycursor.close()
+    mydb.close()
+    print("Database connection closed.")
 
